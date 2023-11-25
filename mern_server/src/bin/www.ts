e203f9eca9d05cd368d53369e72d05a36723c0fa
MODIFYC <chsj5742@naver.com>
2023-11-25 19:27:11 +0900

    mongo+env 수정

--- mern_server/src/bin/www.ts
+++ mern_server/src/bin/www.ts
@@ -18,11 +18,12 @@ app.set('port', port);
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
 
-mongoose.connect(
-  `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authMechanism=DEFAULT&authSource=admin`
-).then(() => console.log("Connected to mongo server"))
+mongoose
+  .connect(
+  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}?retryWrites=true&w=majority`)
+  .then(() => console.log("Connected to mongo server"))
   .catch((e) => console.log(e));
-
+  
 /**
  * Create HTTP server.
  */
